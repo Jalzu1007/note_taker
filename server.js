be39d70fdfb31d0a0699bb689612cc8f1700047a
Jose Alzuri <jalzu1007@gmail.com>
2023-06-29 16:18:14 -0400

    Added server.js file connecting the routes, middleware and starting the server

---
+++ server.js
@@ -0,0 +1,24 @@
+// Required Modules
+const express = require('express');
+const path = require('path');
+const apiRoutes = require('./routes/apiroutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+// API routes
+app.use('/api', apiRoutes);
+
+// HTML routes
+app.use('/', htmlRoutes);
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
